ba956313b0574163691b3aee37055919703f6b97
Saiyadul77 <saiyadul77@gmail.com>
2022-05-30 10:02:07 +0600

    bannar updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
                 <img src="https://i.ibb.co/SyC9wXN/Computer-hardware-components.jpg" className="max-w-sm rounded-lg shadow-2xl h-48 w-full object-cover md:h-full md:w-48 lg:h-full lg:w-96" />
                 <div>
                     <h1 className="text-5xl font-bold">Order online now!!!!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">Akhand Computer Parts is Largest Chain Computer stores in BD, with best prices on Desktops, Laptops, PC Parts, Cameras, Gaming, Printers & more........</p>
                     <button className="btn btn-primary text-white bg-gradient-to-r from-sky-500 to-indigo-500">Get Started</button>
                 </div>
             </div>
